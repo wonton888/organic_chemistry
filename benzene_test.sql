97dc404ba68556800556a57b8f9b1ec37d514d1c
Adam <wonton916@gmail.com>
2015-11-12 14:23:19 -0800

    add sql files to project

---
+++ benzene_test.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8080
+-- Generation Time: Nov 12, 2015 at 10:16 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `benzene_test`
+--
+CREATE DATABASE IF NOT EXISTS `benzene_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `benzene_test`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `benzenes`
+--
+
+DROP TABLE IF EXISTS `benzenes`;
+CREATE TABLE IF NOT EXISTS `benzenes` (
+  `id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `derivatives`
+--
+
+DROP TABLE IF EXISTS `derivatives`;
+CREATE TABLE IF NOT EXISTS `derivatives` (
+  `id` bigint(20) unsigned NOT NULL,
+  `derivative_name` varchar(255) DEFAULT NULL,
+  `benzene_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `benzenes`
+--
+ALTER TABLE `benzenes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `derivatives`
+--
+ALTER TABLE `derivatives`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `benzenes`
+--
+ALTER TABLE `benzenes`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `derivatives`
+--
+ALTER TABLE `derivatives`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
